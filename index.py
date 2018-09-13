194d50c7c7bdd68a81cbec19f4f6fec03c87f1b6
Khalid <khalidjamal11@hotmail.com>
2018-09-13 21:32:41 +0200

    - index

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
-    return "Hello Aisha"
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
