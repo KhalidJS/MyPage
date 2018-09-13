d3401af026b6c4456086bcc04ded1c55d39250ec
Khalid <khalidjamal11@hotmail.com>
2018-09-13 21:54:12 +0200

    - index

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sslify import SSLify
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 @app.route('/')
 def index():
     return "Hello World!"
 
+@app.route('/data')
+def names():
+    data = {"Names": ["Aisha", "Jennifer", "Morten", "Jacob", "John"]}
+    return jsonify(data)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
