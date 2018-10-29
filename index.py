3f9e3d7cab8cd7c6b2870c6d51091d61092f404e
khalid <khalidjamal11@hotmail.com>
2018-10-29 21:01:33 +0100

    # files updated

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form.get('Submit-button') == 'submit':
+            text = request.form['username']
+            password = request.form['password']
+            print("submitted")
+            print("A user with username:" + text + " and with password: ' " + password + " 'has looged in")
     return render_template("login.html")
 
 
